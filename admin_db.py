f20df8bc625ee115b264f9dea01d1f5db377fbd0
schulz-franco <fschulz2222@gmail.com>
2021-11-12 14:00:32 -0300

    db helper, terminado

--- admin_db.py
+++ admin_db.py
@@ -15,33 +15,66 @@ class interfaz_db_helper(QDialog):
         self.ui = Ui_admin_db()
         self.ui.setupUi(self)
 
-        self.cargar_elementos_empleados()
-        self.cargar_elementos_productos()
-        self.cargar_elementos_ventas()
+        self.mostrar_elementos(Producto, self.ui.importe_de_venta_2)
+        self.mostrar_elementos(Vendedor, self.ui.importe_de_venta_4)
+        self.mostrar_elementos(Venta, self.ui.importe_de_venta_6)
 
         self.ui.btn_agregar_carrito.clicked.connect(self.limpiar_tabla_productos)
         self.ui.btn_agregar_carrito_4.clicked.connect(self.limpiar_tabla_empleados)
         self.ui.btn_agregar_carrito_7.clicked.connect(self.limpiar_tabla_ventas)
 
+        self.ui.btn_agregar_carrito_2.clicked.connect(self.cargar_productos)
+        self.ui.btn_agregar_carrito_6.clicked.connect(self.cargar_empleados)
+        self.ui.btn_agregar_carrito_9.clicked.connect(self.cargar_ventas)
+
         self.show()
 
-    def cargar_elementos_productos(self):
+    def cargar_productos(self):
+        try:
+            for i in range(int(self.ui.input_productos.text())):
+                Producto.create(
+                    codigo='1c' + str(i),
+                    descripcion='texto_de_descripcion' + str(i),
+                    precio='200',
+                    stock='50'
+                )
+            self.mostrar_elementos(Producto, self.ui.importe_de_venta_2)
+        except:
+            pass
+
+    def cargar_empleados(self):
+        try:
+            for i in range(int(self.ui.input_empleados.text())):
+                Vendedor.create(
+                    codigo='1f' + str(i),
+                    nombre='example' + str(i),
+                    apellido='example' + str(i),
+                    edad='25',
+                    ventas='0'
+                )
+            self.mostrar_elementos(Vendedor, self.ui.importe_de_venta_4)
+        except:
+            pass
+
+    def cargar_ventas(self):
+        try:
+            for i in range(int(self.ui.input_ventas.text())):
+                Venta.create(
+                    codigo_vendedor='example' + str(i),
+                    importe='250',
+                )
+            self.mostrar_elementos(Venta, self.ui.importe_de_venta_6)
+        except:
+            pass
+
+    def mostrar_elementos(self, modelo, objeto):
         p_el = 0
-        for row in Producto.select():
-            p_el = str(row.id)
-        self.ui.importe_de_venta_2.setText(str(p_el) + ' datos')
-
-    def cargar_elementos_empleados(self):
-        e_el = 0
-        for row in Vendedor.select():
-            e_el = str(row.id)
-        self.ui.importe_de_venta_4.setText(str(e_el) + ' datos')
-
-    def cargar_elementos_ventas(self):
-        v_el = 0
-        for row in Venta.select():
-            v_el = str(row.venta)
-        self.ui.importe_de_venta_6.setText(str(v_el) + ' datos')
+        for row in modelo.select():
+            try:
+                p_el = str(row.id)
+            except:
+                p_el = str(row.venta)
+        objeto.setText(str(p_el) + ' datos')
 
     def limpiar_tabla_productos(self):
         if Producto.table_exists():
